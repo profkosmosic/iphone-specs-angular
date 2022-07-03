b5f6cb79274c8ad731509d961b033b49b943dcb0
profkosmosic <61395703+profkosmosic@users.noreply.github.com>
2022-07-03 16:34:19 +0200

    binding

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from 'src/app/common/global-constants';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  title = GlobalConstants.TITLE;
 
   constructor() { }
 
