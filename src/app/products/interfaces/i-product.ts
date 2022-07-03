9ebb18da8f3a07e7a0877588b8f5a67510209342
profkosmosic <61395703+profkosmosic@users.noreply.github.com>
2022-07-03 17:51:28 +0200

    Update i-product.ts

--- src/app/products/interfaces/i-product.ts
+++ src/app/products/interfaces/i-product.ts
@@ -1,2 +1,10 @@
 export interface IProduct {
+    id:number;
+    name:string;
+    image:string;
+    capacity:number;
+    screenSize:number;
+    screenResolution:string;
+    cpu:string;
+    camera:number;
 }
