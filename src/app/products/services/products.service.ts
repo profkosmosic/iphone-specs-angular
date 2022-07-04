c56231815a174224461a65f3d8a49cf913c6b778
profkosmosic <61395703+profkosmosic@users.noreply.github.com>
2022-07-04 20:59:39 +0200

    Update products.service.ts

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { api } from 'src/app/constants/api';
+import { serverPath } from 'src/app/constants/server';
+import { IProduct } from '../interfaces/i-product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
